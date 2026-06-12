f558e2fd587a599be0dbcfb58ac254c24187c75a
Alweb7 <146018127+Alweb7@users.noreply.github.com>
2026-06-12 16:03:48 +0000

    Jun 12, 2026, 6:03 PM

--- prog ad oggetti/Esercizi/array_demo.java
+++ prog ad oggetti/Esercizi/array_demo.java
@@ -1,5 +1,22 @@
 public class array_demo {
     public static void main(String[] args) {
+        //dichiaro
+        int [] anArray;
+        //inizializzo l'array
+        anArray= new int[10];
+        
+        //riempo l'array senza cicili
+        anArray[0]= 100;
+        anArray[1]= 200;
+        anArray[2]= 300;
+        anArray[3]= 400;
+        anArray[4]= 500;
+
+
+
+        //stampo array
+
+        System.out.println("elemnto 1:"+ anArray[1]);
         
     }
 
