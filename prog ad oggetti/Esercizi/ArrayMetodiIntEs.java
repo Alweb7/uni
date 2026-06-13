614185bbf79e01ca215f329e3cd64d4cc386150e
Alweb7 <146018127+Alweb7@users.noreply.github.com>
2026-06-13 07:45:00 +0000

    Jun 13, 2026, 9:45 AM

--- prog ad oggetti/Esercizi/ArrayMetodiIntEs.java
+++ prog ad oggetti/Esercizi/ArrayMetodiIntEs.java
@@ -23,6 +23,24 @@ public class ArrayMetodiIntEs {
     }
     return true;
     }
+    static boolean find_number(int[] a,int n){
+        
+        for(int i = 0; i < a.length; i++){
+            if (a[i]==n) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static int max_value(int a[]){
+        int max=0;
+        for (int i : a) {
+            if (i>max) {
+                max=i;
+            }
+        }
+        return max;
+    }
 
 
     public static void main(String[] args) {
