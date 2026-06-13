918d166d274b275e987d12fe87ecf0a27e56fefa
Alweb7 <146018127+Alweb7@users.noreply.github.com>
2026-06-13 12:19:24 +0000

    Jun 13, 2026, 2:19 PM

--- prog ad oggetti/Esercizi/Classe_Multiplications.java
+++ prog ad oggetti/Esercizi/Classe_Multiplications.java
@@ -1,7 +1,7 @@
 public class Classe_Multiplications {
     static final int MAX = 10;
 
-    public static int[] Newarray(){
+    public static int[][] Newarray(){
         int[][] a= new int[MAX][MAX];
         for(int i=0;i<MAX;i++){
             for(int j=0;j<MAX;j++){
@@ -9,9 +9,16 @@ public class Classe_Multiplications {
             }
         }
         return a;
-
+    }
+    static void try_stamp(int[][] a){
+        for (int[] is : a) {
+            for (int n : is) {
+                System.out.println(n);
+            }
+        }
     }
     public static void main(String[] args) {
+        try_stamp(Newarray());
         
     }
 }
