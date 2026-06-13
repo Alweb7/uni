bfdd384533c758bb316b9ede3d36ead57e9774fd
Alweb7 <146018127+Alweb7@users.noreply.github.com>
2026-06-13 07:48:01 +0000

    Jun 13, 2026, 9:48 AM

--- prog ad oggetti/Esercizi/ArrayMetodiIntEs.java
+++ prog ad oggetti/Esercizi/ArrayMetodiIntEs.java
@@ -32,18 +32,36 @@ public class ArrayMetodiIntEs {
         }
         return false;
     }
-    static int max_value(int a[]){
-        int max=0;
-        for (int i : a) {
-            if (i>max) {
-                max=i;
-            }
+    static int max_value(int[] a) {
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) max = a[i];
         }
         return max;
     }
 
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int[] a = {3, 7, 1, 9, 4};
+        int[] b = {3, 7, 1, 9, 4};
+        int[] c = {3, 7, 1, 9, 5};
+        int[] negativi = {-5, -2, -8};
+
+        System.out.println("lunghezza: " + lunghezza(a));               // 5
+
+        System.out.println("stampare:");
+        stampare(a);                                                     // 3 7 1 9 4
+
+        System.out.println("somma: " + somma(a));                       // 24
+
+        System.out.println("confrontoDue a==b: " + confrontoDue(a, b)); // true
+        System.out.println("confrontoDue a==c: " + confrontoDue(a, c)); // false
+
+        System.out.println("find_number 9: " + find_number(a, 9));      // true
+        System.out.println("find_number 0: " + find_number(a, 0));      // false
+
+        System.out.println("max_value: " + max_value(a));               // 9
+        System.out.println("max_value negativi: " + max_value(negativi)); // -2
     }
 }
