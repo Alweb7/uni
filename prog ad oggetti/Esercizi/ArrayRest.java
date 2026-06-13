e789d6b7388b124ed7c34cbc878d2595fd350352
Alweb7 <146018127+Alweb7@users.noreply.github.com>
2026-06-13 08:25:27 +0000

    Jun 13, 2026, 10:25 AM

--- prog ad oggetti/Esercizi/ArrayRest.java
+++ prog ad oggetti/Esercizi/ArrayRest.java
@@ -1,6 +1,6 @@
 public class ArrayRest {
     static int[] array(){
-        int[] array=new int[10];
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         return array;
     }
     static void stampo_arry(int[] a){
