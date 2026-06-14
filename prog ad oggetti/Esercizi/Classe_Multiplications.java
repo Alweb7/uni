a1fa808275be891320721b7b2980de3ab1efc751
Alweb7 <146018127+Alweb7@users.noreply.github.com>
2026-06-14 08:18:03 +0000

    Jun 14, 2026, 10:18 AM

--- prog ad oggetti/Esercizi/Classe_Multiplications.java
+++ prog ad oggetti/Esercizi/Classe_Multiplications.java
@@ -24,8 +24,16 @@ public class Classe_Multiplications {
         return prod;
     }
     public static void main(String[] args) {
-        try_stamp(Newarray());
+        int[][] tavola = Newarray();
 
-        
+        System.out.println("=== Tavola pitagorica ===");
+        try_stamp(tavola);
+
+        System.out.println("\n=== prodotto ===");
+        System.out.println("3 x 4 = " + prodotto(3, 4));   // 12
+        System.out.println("5 x 6 = " + prodotto(5, 6));   // 30
+        System.out.println("7 x 7 = " + prodotto(7, 7));   // 49
+        System.out.println("1 x 1 = " + prodotto(1, 1));   // 1
+        System.out.println("10 x 10 = " + prodotto(10, 10)); // 100
     }
 }
